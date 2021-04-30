335c112a119f1dd22ff18d80c7b33279dcc70a71
yangjiakai-isid <xyang.jiakai@contract.isid.co.jp>
2021-04-30 13:03:35 +0900

    Project Create

---
+++ src/views/News.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h1>News</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
